ca0e6eca35321f698b749f46d2d5a7ad43ecca4e
aitorortega03 <aitorortega21@gmail.com>
2025-01-08 14:22:25 +0100

    Missing annotation

--- src/main/java/com/aitorortegadev/games/model/Game.java
+++ src/main/java/com/aitorortegadev/games/model/Game.java
@@ -1,5 +1,6 @@
 package com.aitorortegadev.games.model;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -11,6 +12,7 @@ import lombok.NonNull;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Game {
 
     @Id
